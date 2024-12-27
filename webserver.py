7c46deba2e5f0cbe9b62c22a9edb57b68be36fa8
martin f. krafft <madduck@madduck.net>
2024-12-27 22:29:21 +0100

    Reduce spacing to stay <80 cols

--- webserver.py
+++ webserver.py
@@ -72,22 +72,22 @@ def main(args, logger):
                 entry_query = """
                     select e.id as entryid,
                     p1.id as player1id, p1.firstname as firstname1,
-                        p1.name as name1, l1.name as club1, c1.code as country1,
+                      p1.name as name1, l1.name as club1, c1.code as country1,
                     p2.id as player2id, p2.firstname as firstname2,
-                        p2.name as name2, l2.name as club2, c2.code as country2
+                      p2.name as name2, l2.name as club2, c2.code as country2
                     from
                     (
                     (
+                      (
+                      (
                         (
-                        (
-                            (
-                            Entry e inner join Player p1 on e.player1 = p1.id
-                            )
-                            left outer join Player p2 on e.player2 = p2.id
+                        Entry e inner join Player p1 on e.player1 = p1.id
                         )
-                        left outer join Country c1 on p1.country = c1.id
-                        )
-                        left outer join Country c2 on p2.country = c2.id
+                        left outer join Player p2 on e.player2 = p2.id
+                      )
+                      left outer join Country c1 on p1.country = c1.id
+                      )
+                      left outer join Country c2 on p2.country = c2.id
                     )
                     left outer join Club l1 on p1.club = l1.id
                     )
@@ -100,13 +100,13 @@ def main(args, logger):
                     c.name as courtname, l.name as locationname
                     from
                     (
+                      (
                         (
-                            (
-                            PlayerMatch m inner join Draw d on (m.draw = d.id)
-                            )
-                            inner join Event v on (d.event = v.id)
+                        PlayerMatch m inner join Draw d on (m.draw = d.id)
                         )
-                        left outer join Court c on (m.court = c.id)
+                        inner join Event v on (d.event = v.id)
+                      )
+                      left outer join Court c on (m.court = c.id)
                     )
                     left outer join Location l on (d.location = l.id)
                 """
