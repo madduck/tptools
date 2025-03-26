d294a3b2a92b2999a67b53ef234876e270ab4e74
martin f. krafft <madduck@madduck.net>
2025-03-26 10:58:17 +0100

    Handle platform-specific imports

--- tptools/cli/tpshortcut.py
+++ tptools/cli/tpshortcut.py
@@ -1,8 +1,6 @@
 import click
 import sys
 import pathlib
-from win32com.client import Dispatch
-from win32com.shell import shell, shellcon
 
 LNKTEMPLATE = "TP file to {tool}"
 BATFILESDIR = pathlib.Path(__file__).parent.parent.parent / "winscripts"
@@ -16,8 +14,19 @@ BATFILESDIR = pathlib.Path(__file__).parent.parent.parent / "winscripts"
     show_default=True,
     help="Template to use for link filename",
 )
-@click.argument("script", nargs=1)
+@click.argument("script", nargs=-1)
 def main(template, script):
+    try:
+        from win32com.client import Dispatch
+        from win32com.shell import shell, shellcon
+
+    except ImportError:
+        raise click.Abort("pywin32 is missing. This script only works on Windows.")
+
+    script = "-".join(script)
+    batch = BATFILESDIR / f"{script}.bat"
+    if not batch.exists():
+        raise click.UsageError(f"No such batch file: {batch}")
 
     desktopdir = pathlib.Path(shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, None, 0))
     scriptshell = Dispatch("WScript.Shell")
@@ -27,7 +36,7 @@ def main(template, script):
     dest = desktopdir / lnk
     shortcut = scriptshell.CreateShortcut(str(dest))
     shortcut.WorkingDirectory = str(desktopdir)
-    shortcut.Targetpath = str(BATFILESDIR / f"{script}.bat")
+    shortcut.Targetpath = str(batch)
     shortcut.save()
 
     print(f"Shortcut for {script} created: {dest}")
