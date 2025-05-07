a8052ea5c3366812f181f63c40aeee755f46e75a
martin f. krafft <madduck@madduck.net>
2025-05-07 18:34:21 +0200

    Add limited typing to tpshortcut cli

--- tptools/cli/tpshortcut.py
+++ tptools/cli/tpshortcut.py
@@ -1,6 +1,8 @@
-import click
-import sys
 import pathlib
+import sys
+from typing import Never
+
+import click
 
 LNKTEMPLATE = "TP file to {tool}"
 BATFILESDIR = pathlib.Path(__file__).parent.parent.parent / "winscripts"
@@ -14,32 +16,42 @@ BATFILESDIR = pathlib.Path(__file__).parent.parent.parent / "winscripts"
     show_default=True,
     help="Template to use for link filename",
 )
-@click.argument("script", nargs=-1)
-def main(template, script):
+@click.argument("script", type=click.Path(path_type=pathlib.Path), nargs=-1)
+def main(template: str, script: list[pathlib.Path]) -> None | Never:
     try:
-        from win32com.client import Dispatch
-        from win32com.shell import shell, shellcon
+        from win32com.client import (
+            Dispatch,
+        )
+        from win32com.shell import (
+            shell,
+            shellcon,
+        )
 
-    except ImportError:
-        raise click.Abort("pywin32 is missing. This script only works on Windows.")
+    except ImportError as err:
+        raise click.Abort(
+            "pywin32 is missing. This script only works on Windows."
+        ) from err
 
-    script = "-".join(script)
-    batch = BATFILESDIR / f"{script}.bat"
-    if not batch.exists():
-        raise click.UsageError(f"No such batch file: {batch}")
+    for s in script:
+        batch = BATFILESDIR / f"{s}.bat"
+        if not batch.exists():
+            raise click.UsageError(f"No such batch file: {batch}")
 
-    desktopdir = pathlib.Path(shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, None, 0))
-    scriptshell = Dispatch("WScript.Shell")
-    lnk = template.format(tool=script)
-    if not lnk.endswith(".lnk"):
-        lnk += ".lnk"
-    dest = desktopdir / lnk
-    shortcut = scriptshell.CreateShortcut(str(dest))
-    shortcut.WorkingDirectory = str(desktopdir)
-    shortcut.Targetpath = str(batch)
-    shortcut.save()
+        desktopdir = pathlib.Path(
+            shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, 0, 0)
+        )
+        scriptshell = Dispatch("WScript.Shell")
+        lnk = template.format(tool=s)
+        if not lnk.endswith(".lnk"):
+            lnk += ".lnk"
+        dest = desktopdir / lnk
+        shortcut = scriptshell.CreateShortcut(str(dest))
+        shortcut.WorkingDirectory = str(desktopdir)
+        shortcut.Targetpath = str(batch)
+        shortcut.save()
 
-    print(f"Shortcut for {script} created: {dest}")
+        print(f"Shortcut for {s} created: {dest}")
+    return None
 
 
 if __name__ == "__main__":
