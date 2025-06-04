f64e65474bbcb25d2fe21ccf7ae015015de44384
martin f. krafft <madduck@madduck.net>
2025-06-04 17:47:11 +0200

    MatchMaker to assemble Matches from PlayerMatches

---
+++ tests/test_matchmaker.py
@@ -0,0 +1,292 @@
+from collections.abc import Callable
+from contextlib import nullcontext
+from typing import Any, ContextManager, Protocol
+
+import pytest
+
+from tptools.match import Match
+from tptools.matchmaker import MatchMaker
+from tptools.matchstatus import MatchStatus
+from tptools.models import Entry, PlayerMatch
+
+from .conftest import MatchFactoryType, PlayerMatchFactoryType
+
+
+def test_construction() -> None:
+    _ = MatchMaker()
+
+
+@pytest.fixture
+def matchmaker() -> MatchMaker:
+    return MatchMaker()
+
+
+def test_repr(matchmaker: MatchMaker) -> None:
+    assert repr(matchmaker) == "MatchMaker(nmatches=0, nunmatched=0)"
+
+
+def test_str(matchmaker: MatchMaker) -> None:
+    assert repr(matchmaker) == "MatchMaker(nmatches=0, nunmatched=0)"
+
+
+def test_no_singles_at_start(matchmaker: MatchMaker) -> None:
+    assert len(matchmaker.unmatched) == 0
+
+
+def test_make_match_incomplete(matchmaker: MatchMaker, match1: Match) -> None:
+    matchmaker.add_playermatch(match1.pm1)
+    assert len(matchmaker.unmatched) == 1
+    assert len(matchmaker.matches) == 0
+
+
+def test_make_match(matchmaker: MatchMaker, match1: Match) -> None:
+    matchmaker.add_playermatch(match1.pm2)
+    matchmaker.add_playermatch(match1.pm1)
+    assert len(matchmaker.unmatched) == 0
+    assert len(matchmaker.matches) == 1
+    assert matchmaker.matches.pop() == match1
+
+
+def test_make_match_duplicate(matchmaker: MatchMaker, match1: Match) -> None:
+    matchmaker.add_playermatch(match1.pm1)
+    with pytest.raises(ValueError, match="is already registered"):
+        matchmaker.add_playermatch(match1.pm1)
+
+
+def test_resolve_with_unmatched(matchmaker: MatchMaker, match1: Match) -> None:
+    matchmaker.add_playermatch(match1.pm1)
+    with pytest.raises(AssertionError, match="Cannot resolve entries with unmatched"):
+        matchmaker.resolve_match_entries()
+
+
+def test_resolve_1st_round(
+    matchmaker: MatchMaker,
+    match1: Match,
+    pmplayer1: PlayerMatch,
+    pmplayer2: PlayerMatch,
+) -> None:
+    pm1 = match1.pm1.model_copy(update={"entry": None})
+    pm2 = match1.pm2.model_copy(update={"entry": None})
+    # TODO: should really mock out the Match stuff
+    for pm in (pmplayer1, pmplayer2, pm1, pm2):
+        matchmaker.add_playermatch(pm)
+    matchmaker.resolve_match_entries()
+    match = matchmaker.matches.pop()
+    assert match.slot1.content == pmplayer1.entry
+    assert match.slot2.content == pmplayer2.entry
+    assert match.status == MatchStatus.READY
+
+
+def test_resolve_1st_round_with_bye(
+    matchmaker: MatchMaker,
+    match1: Match,
+    pmplayer1: PlayerMatch,
+    pmbye: PlayerMatch,
+) -> None:
+    pm1 = match1.pm1.model_copy(update={"entry": None})
+    pm2 = match1.pm2.model_copy(update={"entry": None})
+    # TODO: should really mock out the Match stuff
+    for pm in (pmplayer1, pmbye.model_copy(update={"planning": 4002}), pm1, pm2):
+        matchmaker.add_playermatch(pm)
+    matchmaker.resolve_match_entries()
+    match = matchmaker.matches.pop()
+    assert match.status == MatchStatus.READY
+
+
+def test_resolve_unmatched_incomplete(matchmaker: MatchMaker, match1: Match) -> None:
+    matchmaker.add_playermatch(match1.pm1)
+    with pytest.raises(ValueError, match="Cannot resolve unmatched PlayerMatch"):
+        matchmaker.resolve_unmatched()
+
+
+class MatchMakerTripletFactory(Protocol):
+    @classmethod
+    def __call__(
+        cls,
+        updict1: dict[str, Any] | None = None,
+        updict2: dict[str, Any] | None = None,
+        updict3: dict[str, Any] | None = None,
+    ) -> MatchMaker: ...
+
+
+@pytest.fixture
+def matchmaker_with_triplet(
+    matchmaker: MatchMaker,
+    entry1: Entry,
+    entry2: Entry,
+    pmplayer1: PlayerMatch,
+    pmplayer2: PlayerMatch,
+    PlayerMatchFactory: PlayerMatchFactoryType,
+    MatchFactory: MatchFactoryType,
+) -> MatchMakerTripletFactory:
+    matchmaker.add_playermatch(pmplayer1)
+    matchmaker.add_playermatch(pmplayer1.model_copy(update={"planning": 4003}))
+    matchmaker.add_playermatch(pmplayer2)
+    matchmaker.add_playermatch(pmplayer2.model_copy(update={"planning": 4004}))
+
+    def matchmaker_factory(
+        updict1: dict[str, Any] | None = None,
+        updict2: dict[str, Any] | None = None,
+        updict3: dict[str, Any] | None = None,
+    ) -> MatchMaker:
+        srcpm1 = PlayerMatchFactory(
+            **{
+                "matchnr": 1,
+                "planning": 3001,
+                "van1": 4001,
+                "van2": 4002,
+                "wn": 2001,
+                "vn": 2003,
+                "entry": None,
+            }
+            | (updict1 or {})
+        )
+        srcm1 = MatchFactory(srcpm1, entry2=None, lldiff=4)
+
+        srcpm2 = PlayerMatchFactory(
+            **{
+                "matchnr": 2,
+                "planning": 3002,
+                "van1": 4003,
+                "van2": 4004,
+                "wn": 2001,
+                "vn": 2003,
+                "entry": entry1,
+                "winner": 1,
+            }
+            | (updict2 or {})
+        )
+        srcm2 = MatchFactory(srcpm2, entry2=entry2, lldiff=4)
+        srcpm3 = PlayerMatchFactory(
+            **{
+                "matchnr": 3,
+                "planning": 2001,
+                "van1": 3001,
+                "van2": 3002,
+                "wn": 1001,
+                "vn": 1002,
+            }
+            | (updict3 or {})
+        )
+        srcm3 = MatchFactory(srcpm3, entry2=None, lldiff=2)
+
+        for m in (srcm1, srcm2, srcm3):
+            matchmaker.add_playermatch(m.pm1)
+            matchmaker.add_playermatch(m.pm2)
+
+        return matchmaker
+
+    return matchmaker_factory
+
+
+@pytest.mark.parametrize(
+    "updict1, updict2, updict3, expctx",
+    [
+        # the case when planning matches a wn/vn of the first preceding PlayerMatch,
+        # then we are dealing with the winner, but the match is obviously still pending.
+        (
+            None,
+            None,
+            {"planning": 2001},
+            nullcontext(
+                {
+                    "status": lambda s: s == MatchStatus.PENDING,
+                    "slot1": lambda s: str(s).startswith("Winner")
+                    and str(s).endswith("#1"),
+                }
+            ),
+        ),
+        # in this (fake) case, planning is made to match wn/vn of the second preceding
+        # PlayerMatch, then we are dealing with the loser, but the match is obviously
+        # still pending.
+        (
+            None,
+            None,
+            {"planning": 2007},
+            nullcontext(
+                {
+                    "status": lambda s: s == MatchStatus.PENDING,
+                    "slot1": lambda s: str(s).startswith("Loser")
+                    and str(s).endswith("#1"),
+                }
+            ),
+        ),
+        # in this case, the match's planning (2001) is not in either wn/vn of the
+        # preceding matches, and that cannot be, there's an inconsistency.
+        ({"wn": 2005, "vn": 2007}, None, None, pytest.raises(ValueError)),
+    ],
+)
+def test_resolve_2nd_round_players_unknown(
+    matchmaker_with_triplet: MatchMakerTripletFactory,
+    updict1: dict[str, Any] | None,
+    updict2: dict[str, Any] | None,
+    updict3: dict[str, Any] | None,
+    expctx: ContextManager[dict[str, Callable[[Any], bool]]],
+) -> None:
+    matchnr = 42
+    matchmaker = matchmaker_with_triplet(
+        updict1, updict2, (updict3 or {}) | {"matchnr": matchnr}
+    )
+    with expctx as exp:
+        matchmaker.resolve_match_entries()
+
+        for match in matchmaker.matches:
+            if match.matchnr == matchnr:
+                for key, checkfn in exp.items():
+                    assert checkfn(getattr(match, key)), (
+                        f"Assertion of key {key} failed"
+                    )
+
+                break
+
+
+def test_resolve_unmatched_fabricate(
+    matchmaker: MatchMaker,
+    PlayerMatchFactory: PlayerMatchFactoryType,
+) -> None:
+    common = {
+        "matchnr": 1,
+        "van1": 3001,
+        "van2": 3002,
+    }
+    srcpm1 = PlayerMatchFactory(**common | {"planning": 2001, "wn": 1001, "vn": None})
+    srcpm2 = PlayerMatchFactory(
+        **common
+        | {
+            "planning": 2002,
+            "wn": 1003,
+            "vn": 1004,
+        }
+    )
+    pm = PlayerMatchFactory(matchnr=2, planning=1001, van1=2001, van2=2002)
+
+    matchmaker.add_playermatch(srcpm1)
+    matchmaker.add_playermatch(srcpm2)
+    matchmaker.add_playermatch(pm)
+
+    matchmaker.resolve_unmatched()
+
+    assert len(matchmaker.matches) == 2
+    assert len(matchmaker.unmatched) == 0
+
+
+def test_resolve_unmatched_fabricate_1st_round(
+    matchmaker: MatchMaker,
+    pmplayer1: PlayerMatch,
+    pmplayer2: PlayerMatch,
+    PlayerMatchFactory: PlayerMatchFactoryType,
+) -> None:
+    pmplayer1 = pmplayer1.model_copy(update={"vn": None})
+    pmplayer2 = pmplayer2.model_copy(update={"vn": None})
+
+    pm = PlayerMatchFactory(
+        matchnr=1, planning=3001, van1=pmplayer1.planning, van2=pmplayer2.planning
+    )
+
+    for p in (pmplayer1, pmplayer2, pm):
+        matchmaker.add_playermatch(p)
+
+    matchmaker.resolve_unmatched()
+
+    assert len(matchmaker.unmatched) == 0
+    assert len(matchmaker.matches) == 1
---
+++ tptools/matchmaker.py
@@ -0,0 +1,164 @@
+import logging
+from functools import partial
+from typing import cast
+
+from .match import Match
+from .mixins.repr import ReprMixin
+from .models import Draw, PlayerMatch
+from .playermatchstatus import PlayerMatchStatus
+from .slot import Bye, Playceholder, Slot
+
+logger = logging.getLogger(__name__)
+
+
+class MatchMaker(ReprMixin):
+    def __init__(self) -> None:
+        self._unmatched: dict[tuple[Draw, int], PlayerMatch] = {}
+        self._matches: dict[tuple[Draw, int], Match] = {}
+        self._players: dict[tuple[Draw, int], PlayerMatch] = {}
+        self._planning_map: dict[tuple[Draw, int], Match] = {}
+
+    def _attr_len_repr(self, name: str) -> str:
+        return str(len(getattr(self, name)))
+
+    __repr_fields__ = (
+        ("nmatches", partial(_attr_len_repr, name="matches"), False),
+        ("nunmatched", partial(_attr_len_repr, name="unmatched"), False),
+    )
+
+    def add_playermatch(self, playermatch: PlayerMatch) -> None:
+        if playermatch.status in (PlayerMatchStatus.PLAYER, PlayerMatchStatus.BYE):
+            self._players[playermatch.draw, playermatch.planning] = playermatch
+            return
+
+        if (
+            other := self._unmatched.pop((playermatch.draw, playermatch.matchnr), None)
+        ) is not None:
+            if playermatch == other:
+                raise ValueError(f"{other} is already registered")
+
+            logger.debug(f"Found match for {playermatch!r}: {other!r}")
+            match = Match(pm1=playermatch, pm2=other)
+            self._matches[playermatch.draw, playermatch.matchnr] = match
+
+            self._planning_map[match.draw, playermatch.planning] = match
+            self._planning_map[match.draw, other.planning] = match
+        else:
+            logger.debug(f"No pair yet for {playermatch!r}, we will keep looking")
+            self._unmatched[playermatch.draw, playermatch.matchnr] = playermatch
+
+        return None
+
+    @property
+    def unmatched(self) -> set[PlayerMatch]:
+        return set(self._unmatched.values())
+
+    @property
+    def matches(self) -> set[Match]:
+        return set(self._matches.values())
+
+    def resolve_unmatched(self) -> None:
+        for pm in sorted(self.unmatched):
+            # Gross hack ahead! In draws without e.g. 3/4 playoffs, there are actually
+            # not pairs of PlayerMatches. Therefore, we fabricate a fake one, since the
+            # Match class expects to work with pairs. The source PlayerMatches will have
+            # wv but no vn pointers.
+            #
+            # First, get any of the preceding player matches. It does not matter which
+            # one as the vn pointers should be the same for both. The preceding
+            # PlayerMatch is either a match, in which case we obtain pm1, or a player
+            srcpm: PlayerMatch | None
+            if (
+                srcmatch := self._planning_map.get((pm.draw, cast(int, pm.van1)))
+            ) is not None:
+                srcpm = srcmatch.pm1
+            else:  # no match found, let's fall back to players
+                srcpm = self._players.get((pm.draw, cast(int, pm.van1)))
+
+            if srcpm is not None:
+                if (
+                    srcpm.wn is not None and srcpm.vn is None
+                ):  # pragma: nocover — there is a missed branch here, but I cannot be
+                    # bothered.
+                    logger.info(f"Fabricating a PlayerMatch to match single {pm!r}")
+                    self.add_playermatch(
+                        pm.model_copy(
+                            update={
+                                "id": -pm.id,
+                                "planning": pm.planning + 10000000,
+                                "wn": (pm.wn + 10000000) if pm.wn is not None else None,
+                                "winner": ((3 - pm.winner) % 3)
+                                if pm.winner is not None
+                                else None,
+                                "entry": None,
+                            }
+                        )
+                    )
+
+            else:
+                raise ValueError(f"Cannot resolve unmatched {pm!r}")
+
+    def resolve_match_entries(self) -> None:
+        assert len(self.unmatched) == 0, (
+            "Cannot resolve entries with unmatched PlayerMatches"
+        )
+
+        # Oh my god, what a mess. The TP "database" is a fucking disgrace, full of
+        # redundancy and corner cases and inconsistencies.
+        #
+        # It is entirely unclear what kind of herb the developers of TournamentSoftware
+        # were smoking when they designed the database, but I am going to wager that (a)
+        # there was no "design" phase anyway, and (b) database, haha. It is remarkable
+        # that this shit works.
+        #
+        # In a nutshell: a "PlayerMatch" is either a player or half of a match, or a
+        # match actually. Complete bonkers. Anyway. because of this, we need to
+        # dual-pass matches, and resolve the entries on second pass, only to be able to
+        # say things like "Martin vs. the Loser of match #4" while match #4 is
+        # undecided.
+        #
+        # To do this, we need to backstep the linked-list created with van1/van2 →
+        # planning, rely on the appearance that iff a match is composed of two
+        # PlayerMatches, the first actually represents the winner OF THE FUCKING
+        # PREVIOUS ROUND, and use the wn/vn forward-pointers to determine this.
+        #
+        # Without further ado…
+
+        for match in sorted(self.matches):
+            slots: list[Slot] = []
+            for van in (match.pm1.van1, match.pm1.van2):
+                if (srcmatch := self._planning_map.get((match.draw, van or 0))) is None:
+                    # There is actually no previous match, so the van pointer points at
+                    # a player or a bye.
+                    entry = self._players[match.draw, van or 0].entry
+                    # if a player, then use the entry, but if the entry is None, then
+                    # it's a bye, and we shouldn't have to be dealing with this sort of
+                    # detail outside the PlayerMatch class, but it's all fucked up
+                    # anyway, so it shall be, and we make a BaseEntry with the "bye"
+                    # player:
+                    slots.append(Slot(content=entry or Bye()))
+
+                else:
+                    # In the case there is a previous match, let's figure out if we come
+                    # from its first or second PlayerMatch. If the former, then we are
+                    # dealing with the winner. Otherwise the loser:
+                    if match.pm1.planning in (srcmatch.pm1.wn, srcmatch.pm1.vn):
+                        slots.append(
+                            Slot(
+                                content=srcmatch.pm1.entry
+                                or Playceholder(matchnr=srcmatch.matchnr, winner=True)
+                            )
+                        )
+
+                    elif match.pm1.planning in (srcmatch.pm2.wn, srcmatch.pm2.vn):
+                        slots.append(
+                            Slot(
+                                content=srcmatch.pm2.entry
+                                or Playceholder(matchnr=srcmatch.matchnr, winner=False)
+                            )
+                        )
+
+                    else:
+                        raise ValueError(f"{match} does not derive from {srcmatch}")
+
+            match.set_slots(*slots)
