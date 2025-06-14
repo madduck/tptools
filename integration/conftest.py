4f4b438aba5f24272347bcf0f9a70b8541e58590
martin f. krafft <madduck@madduck.net>
2025-06-14 15:46:36 +0200

    Enable testing against TP file if present on Windows

--- integration/conftest.py
+++ integration/conftest.py
@@ -1,23 +1,44 @@
 import pathlib
+import sys
 from collections.abc import Generator
 from typing import Any
 
 import pytest
-from sqlalchemy import ScalarResult
+from sqlalchemy.engine import URL
 from sqlmodel import Session, create_engine, select
 
 from tptools.match import Match
 from tptools.matchmaker import MatchMaker
 from tptools.models import Entry, PlayerMatch
 from tptools.playermatchstatus import PlayerMatchStatus
+from tptools.util import make_mdb_odbc_connstring
+
+DB_PATH_BASE = pathlib.Path(__file__).parent / "anon_tournament"
+if sys.platform == "win32" and (tp_path := DB_PATH_BASE.with_suffix(".TP")).exists():  # noqa: F841
+    # could use pytest_addoption to export the database name to test against,
+    # but why bother? Noone in their right mind develops on Windows, at least
+    # I do not, and so if pytest runs on Windows *and* the TP file is present,
+    # run all tests against that. Seems good enough.
+    connection_url = URL.create(
+        "access+pyodbc",
+        query={
+            "odbc_connect": make_mdb_odbc_connstring(
+                tp_path, uid="Admin", pwd="d4R2GY76w2qzZ"
+            )
+        },
+    )  # TODO: hide the password
+else:
+    db_path = DB_PATH_BASE.with_suffix(".sqlite")
+    connection_url = URL.create("sqlite", database=str(db_path))
+
+
+def pytest_report_header(config: pytest.Config) -> str | list[str]:
+    return f"Connection string for integration tests: {connection_url}"
 
 
 @pytest.fixture(scope="session")
-def sqlite_ro_session(
-    sqlite_db_path: pathlib.Path = pathlib.Path(__file__).parent
-    / "anon_tournament.sqlite",
-) -> Generator[Session, Any]:
-    engine = create_engine(f"sqlite:///{sqlite_db_path}")
+def db_session() -> Generator[Session, Any]:
+    engine = create_engine(connection_url)
     session = Session(engine)
     session.begin()
     yield session
@@ -31,7 +52,7 @@ def sqlite_ro_session(
     # session.close()
     import atexit
 
-    def teardown():
+    def teardown() -> None:
         session.rollback()
         session.close()
 
@@ -39,13 +60,13 @@ def sqlite_ro_session(
 
 
 @pytest.fixture
-def all_entries(sqlite_ro_session: Session) -> Generator[list[Entry], Any]:
-    yield list(sqlite_ro_session.exec(select(Entry)))
+def all_entries(db_session: Session) -> Generator[list[Entry], Any]:
+    yield list(db_session.exec(select(Entry)))
 
 
 @pytest.fixture
-def all_playermatches(sqlite_ro_session: Session) -> Generator[list[PlayerMatch], Any]:
-    yield list(sqlite_ro_session.exec(select(PlayerMatch)))
+def all_playermatches(db_session: Session) -> Generator[list[PlayerMatch], Any]:
+    yield list(db_session.exec(select(PlayerMatch)))
 
 
 @pytest.fixture
