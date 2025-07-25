da7499d7cf14a6cc8b8e96cf7458a446addafba2
martin f. krafft <madduck@madduck.net>
2025-07-25 15:08:23 +0200

    Script to wrap inotify watcher running pytest

---
+++ watchtests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+pytest_args=-s
+files=
+reset=0
+for arg in "$@"; do
+  case "$arg//$reset" in
+  --cov*//0)
+    pytest_args="${pytest_args:+$pytest_args }--cov-reset $arg"
+    reset=1
+    ;;
+  -*) pytest_args="${pytest_args:+$pytest_args }$arg" ;;
+  *) files="${files:+$files }$arg" ;;
+  esac
+done
+
+if [ -n "$files" ] && [ $reset -eq 0 ]; then
+  pytest_args="${pytest_args:+$pytest_args }--no-cov"
+fi
+
+(sleep 0.5 && touch conftest.py) &
+exec inotify-hookable -q -t 500 -w . -i '\a\..+\z' -i '__pycache__' -C ".+\.py\$=pytest $pytest_args $files"
