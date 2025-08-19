253e6eee7a90b2780e2a5ef3e8d66aa5ceb2bfb9
martin f. krafft <madduck@madduck.net>
2025-08-20 00:36:48 +0200

    tpsrv batch file improvements

--- winscripts/tpsrv-tp.bat
+++ winscripts/tpsrv-tp.bat
@@ -7,9 +7,13 @@ if not defined tpfile (
   exit /b 1
 )
 
-if not exist %tpfile% (
+for %%a in ("%tpfile%") do set "tpfile=%%~fa"
+
+if not exist "%tpfile%" (
   echo TP file %tpfile% does not exist, first argument must be a TP file
   exit /b 1
 )
 
-tpsrv tp %tpfile%
+tpsrv -v squore post --url http://tc/tp/tournament tp "%tpfile%
+
+pause
