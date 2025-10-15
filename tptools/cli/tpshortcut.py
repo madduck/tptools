21571fd15e467f9366af8b67dbc0f586db24f4b6
martin f. krafft <madduck@madduck.net>
2025-10-15 22:07:36 +0200

    Fixup tpshortcut script for Windows and Python packaging

--- tptools/cli/tpshortcut.py
+++ tptools/cli/tpshortcut.py
@@ -1,3 +1,4 @@
+import importlib.resources
 import pathlib
 import sys
 from typing import Never
@@ -5,7 +6,6 @@ from typing import Never
 import click
 
 LNKTEMPLATE = "TP file to {tool}"
-BATFILESDIR = pathlib.Path(__file__).parent.parent.parent / "winscripts"
 
 
 @click.command
@@ -32,26 +32,27 @@ def main(template: str, script: list[pathlib.Path]) -> None | Never:
             "pywin32 is missing. This script only works on Windows."
         ) from err
 
-    for s in script:
-        batch = BATFILESDIR / f"{s}.bat"
-        if not batch.exists():
-            raise click.UsageError(f"No such batch file: {batch}")
+    with importlib.resources.path("tptools", "winscripts") as winscripts_path:
+        for s in script:
+            batch = winscripts_path / f"{s}.bat"
+            if not batch.exists():
+                raise click.UsageError(f"No such batch file: {batch}")
 
-        desktopdir = pathlib.Path(
-            shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, 0, 0)
-        )
-        scriptshell = Dispatch("WScript.Shell")
-        lnk = template.format(tool=s)
-        if not lnk.endswith(".lnk"):
-            lnk += ".lnk"
-        dest = desktopdir / lnk
-        shortcut = scriptshell.CreateShortcut(str(dest))
-        shortcut.WorkingDirectory = str(desktopdir)
-        shortcut.Targetpath = str(batch)
-        shortcut.save()
+            desktopdir = pathlib.Path(
+                shell.SHGetFolderPath(0, shellcon.CSIDL_DESKTOP, 0, 0)
+            )
+            scriptshell = Dispatch("WScript.Shell")
+            lnk = template.format(tool=s)
+            if not lnk.endswith(".lnk"):
+                lnk += ".lnk"
+            dest = desktopdir / lnk
+            shortcut = scriptshell.CreateShortcut(str(dest))
+            shortcut.WorkingDirectory = str(desktopdir)
+            shortcut.Targetpath = str(batch)
+            shortcut.save()
 
-        print(f"Shortcut for {s} created: {dest}")
-    return None
+            print(f"Shortcut for {s} created: {dest}")
+        return None
 
 
 if __name__ == "__main__":
