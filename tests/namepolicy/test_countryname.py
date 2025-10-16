dfa09033ce4092235eec048cdac3d7d812f152ab
martin f. krafft <madduck@madduck.net>
2025-10-16 12:29:24 +0200

    Add missing tests for CountryNamePolicy code instead of name

--- tests/namepolicy/test_countryname.py
+++ tests/namepolicy/test_countryname.py
@@ -4,6 +4,7 @@ import pytest
 
 from tptools.entry import Country
 from tptools.namepolicy import CountryNamePolicy
+from tptools.namepolicy.policybase import RegexpSubstTuple
 
 
 @pytest.fixture
@@ -44,3 +45,27 @@ def test_code_instead_of_name_uses_name_when_code_absent(
 ) -> None:
     policy = replace(policy, use_country_code=True)
     assert policy(country2) == "Deutschland"
+
+
+def test_code_instead_of_name_warns_when_titlecase(
+    policy: CountryNamePolicy, country1: Country, caplog: pytest.LogCaptureFixture
+) -> None:
+    policy = replace(policy, use_country_code=True, titlecase=True)
+    _ = policy(country1)
+    assert caplog.messages[0].startswith("Ignoring CountryNamePolicy")
+
+
+def test_code_instead_of_name_warns_when_regexp(
+    policy: CountryNamePolicy, country1: Country, caplog: pytest.LogCaptureFixture
+) -> None:
+    policy = replace(policy, use_country_code=True, regexps=[RegexpSubstTuple("", "")])
+    _ = policy(country1)
+    assert caplog.messages[0].startswith("Ignoring CountryNamePolicy")
+
+
+def test_code_instead_no_warns(
+    policy: CountryNamePolicy, country1: Country, caplog: pytest.LogCaptureFixture
+) -> None:
+    policy = replace(policy, use_country_code=True, regexps=None, titlecase=False)
+    _ = policy(country1)
+    assert not caplog.messages
